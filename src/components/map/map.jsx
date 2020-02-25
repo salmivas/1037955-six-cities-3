79568dbd6f59fb6f4e1611dfcf22cdb941f36f3c
Ivan Salmin <salmin.iv@gmail.com>
2020-02-25 16:23:54 +0400

    Adds position points to the map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,46 +1,67 @@
-import React, {PureComponent} from "react";
-import PropTypes from "prop-types";
+import React, {PureComponent, createRef} from "react";
 import L from "leaflet";
+import PropTypes from "prop-types";
 
 class Map extends PureComponent {
   constructor(props) {
     super(props);
 
-    this._mapRef = React.createRef();
-    this.state = {
+    this._city = [52.38333, 4.9];
+    this._zoomMap = 12;
 
+    this._mapConfig = {
+      center: this._city,
+      zoom: this._zoomMap,
+      zoomControl: false,
+      marker: true
     };
-  }
-
-  componentDidMount() {
-    const city = [52.38333, 4.9];
-    const icon = L.icon({
+    this._iconConfig = L.icon({
       iconUrl: `../../../public/img/pin.svg`,
       iconSize: [30, 30]
     });
-    const zoom = 12;
-    const map = L.map(`map`, {
-      center: city,
-      zoom,
-      zoomControl: false,
-      marker: true
-    });
 
-    map.setView(city, zoom);
-    L.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    }).addTo(map);
+    this._mapRef = createRef();
+    this._map = null;
+  }
 
-    const offerCords = [52.3709553943508, 4.89309666406198];
-    L.marker(offerCords, {icon}).addTo(map);
-    this._mapRef.current = map;
+  componentDidMount() {
+    const {points} = this.props;
+
+    if (this._mapRef.current) {
+      this._map = L.map(this._mapRef.current, this._mapConfig);
+      this._map.setView(this._city, this._zoomMap);
+
+      L
+        .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+        })
+        .addTo(this._map);
+
+      points.map((it) => {
+        L
+        .marker(it, this._iconConfig)
+        .addTo(this._map);
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this._map = null;
   }
 
   render() {
     return (
-      <div id="map" ref = {this._mapRef}></div>
+      <section className="cities__map map" ref={this._mapRef} />
     );
   }
 }
+
+Map.propTypes = {
+  points: PropTypes.arrayOf(
+      PropTypes.arrayOf(
+          PropTypes.number.isRequired
+      ).isRequired
+  ).isRequired
+};
 
 export default Map;
